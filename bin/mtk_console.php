d7bd1186f60c6323ec32c73ba00ccf4962386913
visa4 <antonino.visalli@gmail.com>
2015-04-21 12:40:12 +0200

    Fix console entry point

--- bin/mtk_console.php
+++ bin/mtk_console.php
@@ -2,23 +2,23 @@
 use Zend\Mvc\Application;
 use Zend\Stdlib\ArrayUtils;
 
-ini_set('display_errors', true];
+ini_set('display_errors', true);
 
 // Init autoloader
 switch (true) {
-    case is_readable('init_autoloader.php'] :
+    case is_readable('init_autoloader.php') :
         include_once 'init_autoloader.php';
         break;
-    case file_exists(__DIR__ . '/../vendor/autoload.php'] :
+    case file_exists(__DIR__ . '/../vendor/autoload.php') :
         include_once __DIR__ . '/../vendor/autoload.php';
         break;
-    case file_exists(__DIR__ . '/../../../autoload.php'] :
+    case file_exists(__DIR__ . '/../../../autoload.php') :
         include_once __DIR__ . '/../../../autoload.php';
         break;
     default :
         throw new RuntimeException(
             'Error: vendor/autoload.php could not be found. Did you run php composer.phar install?'
-        ];
+        );
 }
 
 // Load config
@@ -29,24 +29,16 @@ switch (true) {
     case file_exists('config/console.config.php') :
         $config = include_once 'config/console.config.php';
         break;
+    case file_exists(__DIR__ . '/../config/console.config.php') :
+        $config = include_once __DIR__ . '/../config/console.config.php';
+        break;
 }
 
 if (isset($config['modules']) && !isset($config['modules']['Matryoshka\Scafolding'])) {
-    $appConfig = [
-        'modules' => [
-            'Matryoshka\Scafolding',
-        ],
-        'module_listener_options' => [
-            'config_glob_paths'    => [
-                'config/autoload/{,*.}{global,local}.php',
-            ],
-            'module_paths' => [
-                '.',
-                './vendor',
-            ],
-        ],
-    ];
-    $config = ArrayUtils::merge($config, $appConfig);
+    if (file_exists(__DIR__ . '/../config/console.config.php')) {
+        $moduleConfig = include_once __DIR__ . '/../config/console.config.php';
+        $config = ArrayUtils::merge($config, $moduleConfig);
+    }
 }
 
 
